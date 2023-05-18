b71c4250e4f641487f897072e755cbf8394dd0ad
G284322022 <228432@st.takushoku-u.ac.jp>
2023-05-18 12:55:24 +0900

    kazuate.jsを変更

--- kazuate.js
+++ kazuate.js
@@ -15,9 +15,21 @@ hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+  kaisu = kaisu + 1;
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+   console.log(kaisu + '回目の予想' + yoso);
+  if (kaisu > 3) {
+    console.log('答えは ' + kotae + ' でした. すでにゲームは終わっています')
+  } else {
+    if (kotae === yoso) {
+      console.log('正解です.おめでとう！');
+    } else if (kotae > yoso) {
+      console.log('まちがい. 答えはもっと大きいですよ'); 
+    } else {
+      console.log('まちがい. 答えはもっと小さいですよ');
+    }
+  }
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
